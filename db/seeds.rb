d179e862b194c5487cd297ae7a48710e4fff7eb1
Xita Rps <xitarps@gmail.com>
2021-08-15 20:54:00 -0300

    Add Scaffold Games
    rails g scaffold Game title:string style:string quantity:integer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Game.new(title: 'Alan Wake', style: 'horror', quantity: 19)
+Game.new(title: 'Bioshock 1', style: 'horror fps', quantity: 22)
+Game.new(title: 'Counter Strike 1.6', style: 'fps', quantity: 13)
