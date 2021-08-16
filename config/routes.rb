f66085d14f2874de2e1e3bfebda4178b8ba484e5
Xita Rps <xitarps@gmail.com>
2021-08-15 21:15:56 -0300

    Update games api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'games#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      get 'games/index'
+      post 'games/create'
+      delete 'games/:id', to: 'games#destroy'
+    end
+  end
 end
